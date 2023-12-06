ea4b228764ad6752998647e030cc6e73732e2cb1
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-12-06 22:28:39 +0530

    created separate file for navbar and bootstrap

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Pondicherry University</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="index.php">Home </a>
+            <a class="nav-item nav-link" href="admit.php">Admit Students</a>
+            <a class="nav-item nav-link" href="feepayment.php">Pay Fees</a>
+            <a class="nav-item nav-link" href="studentdetails.php">Student Details</a>
+        </div>
+    </div>
+</nav>
