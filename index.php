90dd1ddd9228a6d128d8fd68a7c1fc4fc955d633
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-12-07 20:50:04 +0530

    added home page , added logo, added styles, created contactus.php

--- index.php
+++ index.php
@@ -11,6 +11,33 @@
 
 <body>
   <?php include('navbar.php'); ?>
+
+  <div class="container">
+    <div>
+      <img class="pulogo" src="./assets/img/pulogo.png" alt="pulogo">
+    </div>
+    <br>
+    <h1>Welcome to Pondicherry University</h1>
+    <p>Pondicherry University is a central university in Puducherry, India. Established by an Act of Parliament in 1985,
+      it offers a wide range of undergraduate and postgraduate programs across various disciplines.</p>
+
+    <h2>Our Vision</h2>
+    <p>To provide quality education and promote research in diverse fields, fostering a spirit of innovation and global
+      understanding.</p>
+
+    <h2>Admission Information</h2>
+    <p>If you are interested in joining Pondicherry University, explore our programs and admission process on our
+      website. We look forward to welcoming talented students to our vibrant academic community.</p>
+
+    <hr>
+    <a href="https://www.google.com"><button type="button" class="btn btn-primary">Admission</button></a>
+    <button type="button" class="btn btn-warning">Pay Fees</button>
+    <button type="button" class="btn btn-success">Student Details</button>
+    <button type="button" class="btn btn-info">Contact us</button>
+
+
+  </div>
+
 </body>
 
 </html>
--- navbar.php
+++ navbar.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Pondicherry University</a>
+<a class="navbar-brand" href="index.php">
+    <img src="./assets/img/pulogo.png" alt="pulogo" style="height: 35px;margin-right: 10px;">
+    Pondicherry University</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,6 +12,7 @@
             <a class="nav-item nav-link" href="admit.php">Admit Students</a>
             <a class="nav-item nav-link" href="feepayment.php">Pay Fees</a>
             <a class="nav-item nav-link" href="studentdetails.php">Student Details</a>
+            <a class="nav-item nav-link" href="contactus.php">Contact us</a>
         </div>
     </div>
 </nav>
