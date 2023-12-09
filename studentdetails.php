1792d56be330d4c427b3a40c9be666fb89192aeb
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-12-10 00:54:34 +0530

    SUBMIT

--- studentdetails.php
+++ studentdetails.php
@@ -15,6 +15,58 @@ $currentPage = 'studentdetails.php';
 
 <body>
   <?php include('navbar.php'); ?>
+
+  <div class="container">
+    <h1>Student Details</h1>
+
+    <?php
+    // Include the database connection
+    include('dbconnection.php');
+
+    // Fetch all student records
+    $query = "SELECT * FROM students";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+    ?>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone Number</th>
+            <th>Gender</th>
+            <th>Date of Birth</th>
+            <th>Address</th>
+            <th>Course</th>
+            <th>Country</th>
+            <th>Fee Payment</th>
+            <th>Due Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+              <td><?php echo isset($row['name']) ? $row['name'] : ''; ?></td>
+              <td><?php echo isset($row['email']) ? $row['email'] : ''; ?></td>
+              <td><?php echo isset($row['phone_number']) ? $row['phone_number'] : ''; ?></td>
+              <td><?php echo isset($row['gender']) ? $row['gender'] : ''; ?></td>
+              <td><?php echo isset($row['date_of_birth']) ? $row['date_of_birth'] : ''; ?></td>
+              <td><?php echo isset($row['address']) ? $row['address'] : ''; ?></td>
+              <td><?php echo isset($row['course']) ? $row['course'] : ''; ?></td>
+              <td><?php echo isset($row['country']) ? $row['country'] : ''; ?></td>
+              <td><?php echo isset($row['feepayment']) ? ($row['feepayment'] ? 'Paid' : 'Not Paid') : ''; ?></td>
+              <td><?php echo isset($row['due_date']) ? $row['due_date'] : ''; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    <?php } else { ?>
+      <p>No student records found.</p>
+    <?php } ?>
+
+  </div>
+
 </body>
 
 </html>
